b7aab1a32d90c1369f05bac67d0c3e5b953beecc
Melanie Hall <hallmt313@gmail.com>
2021-02-01 14:33:23 -0500

    mongo connected

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 //this syntax, rather tham import, is called common JS since we dont have typescript or babble set up
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 // app.get('/', (req, res) => res.json({msg: "welcome to CK API"}));
+
+//Connect Database
+connectDB();
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
