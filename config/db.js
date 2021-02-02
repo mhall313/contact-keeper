7cbaedf05298e6e0e0b0f457ca1edef25483f812
Melanie Hall <hallmt313@gmail.com>
2021-02-01 19:06:34 -0500

    route to get all contacts

--- config/db.js
+++ config/db.js
@@ -3,20 +3,18 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
-            });
-            console.log("mongodb connected");
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-    
-        
+  try {
+    mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log('mongodb connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
---
+++ models/Contact.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+  user: {
+    //this will set the type to the unique identifier than mongoose applies to each user in the collection "users"
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: 'personal',
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('contact', ContactSchema);
